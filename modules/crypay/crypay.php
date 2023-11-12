4535dd77f49c92ca062c11aae1b58b89bfa7190f
kamelot <ssd@dsdsds.ru>
2023-11-12 16:53:17 +0100

    fix

--- modules/crypay/crypay.php
+++ modules/crypay/crypay.php
@@ -24,7 +24,7 @@ class Crypay extends PaymentModule
     {
         $this->name = 'crypay';
         $this->tab = 'payments_gateways';
-        $this->version = '1.0.3';
+        $this->version = '1.0.4';
         $this->author = 'CryPay.com';
         $this->is_eu_compatible = 1;
         $this->controllers = array('payment', 'redirect', 'callback', 'cancel', 'success');
